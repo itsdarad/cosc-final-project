639d2d973153581851330c334c051313cbfcdf3d
Oluwadara Dina <>
2021-05-09 02:12:04 +0000

    Added a file, started with the base class

---
+++ test.py
@@ -0,0 +1,15 @@
+class Laptop:
+  def __init__(self, processor,):
+    self.__square_feet = sf
+
+  def set_square_feet(self, sf):
+    self.__square_feet = sf
+
+  def set_story(self, st):
+    self.__story = st
+
+  def get_square_feet(self):
+    return self.__square_feet
+
+  def get_story(self):
+    return self.__story
