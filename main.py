d9c548b54dbf909447ddacd72952d2677a624318
Oluwadara Dina <>
2021-05-12 00:00:29 +0000

    Included the def main function and if statements for the user to choose the prices

---
+++ main.py
@@ -0,0 +1,20 @@
+import test
+
+def main():
+  print('Hello! Welcome to your the laptop picker space.')
+
+  print('Down below, you will be picking the best choice computer for your needs.')
+
+  user_prices = int(input("What price range would you like betwen $2000 "))
+
+  if user_prices >= 250 < 500:
+    print("Great Choice")
+
+  elif user_prices >= 500 < 750:
+    print("Hello!")
+    
+  elif user_prices >= 750 < 1000:
+    print('Hello')
+    
+  elif user_prices >= 1250 <= 2000:
+    print("Hello!")
